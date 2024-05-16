2b9483623af135aa703ea3259c72522504555aa1
skramana1973 <skramana1973@gmail.com>
2024-05-15 22:59:30 -0700

    checking gpt40 video analytics capabilities initial poc

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # gpt4o-video-analysis
+
+### STEP 01- Create a conda environment after opening the repository
+
+```bash
+#conda create -n gpt4o-video-analysis python=3.10 -y
+conda create -n gpt4o-video-analysis python=3.11.7 -y
+```
+
+```bash
+conda activate gpt4o-video-analysis
+```
+
+
+### STEP 02- install the requirements
+```bash
+pip install -r requirements.txt
+```
+
+### STEP 04- copy any video file into data directory and change file name accordingly to match with code.
+
+```bash
+data/sample_video.mp4
+```
+
+### STEP 05- Finally run the following command
+
+```bash
+python gpt4o-vision-video-analysis.py
+```
---
+++ gpt4o-vision-video-analysis.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import cv2
+import base64
+import os
+
+load_dotenv()
+
+key = os.getenv("OPENAI_API_KEY")
+# print("OPENAI_API_KEY   :   ", key)
+client = OpenAI(api_key=key)
+video = cv2.VideoCapture("data/sample_video.mp4")
+
+# Extract frames from video
+base64Frames = []
+while video.isOpened():
+    success, frame = video.read()
+    if not success:
+        break
+    _, buffer = cv2.imencode(".jpg", frame)
+    base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
+
+video.release()
+print(len(base64Frames), "frames read.\n")
+
+# Create chat prompt
+PROMPT_MESSAGES = [
+    {
+        "role": "user",
+        "content": [
+            "These are frames from a video that I want to depict. Explain what is in the video in a summary paragraph.",
+            *map(lambda x: {"image": x, "resize": 768}, base64Frames[0::150]),
+        ],
+    },
+]
+
+params = {
+    "model": "gpt-4o",
+    "messages": PROMPT_MESSAGES,
+    "max_tokens": 300,
+}
+
+result = client.chat.completions.create(**params)
+print(result.choices[0].message.content)
